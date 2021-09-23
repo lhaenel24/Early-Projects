a8969fb862be640a8590f7133551eac4691ae879
lhaenel24 <90847601+lhaenel24@users.noreply.github.com>
2021-09-23 08:10:59 -0500

    Add files via upload

---
+++ Calc.py
@@ -0,0 +1,31 @@
+operation = input('''
+Would you like to add, subract, multiply, or divide?
++ to add
+- to subtract
+* to multiply
+/ to divide
+''')
+
+number_1 = int(input('Enter your first number: '))
+number_2 = int(input('Enter your second number: '))
+
+if operation == '+':
+    print('{} + {} = '.format(number_1, number_2))
+    print(number_1 + number_2)
+
+elif operation == '-':
+    print('{} - {} = '.format(number_1, number_2))
+    print(number_1 - number_2)
+
+elif operation == '*':
+    print('{} * {} = '.format(number_1, number_2))
+    print(number_1 * number_2)
+
+elif operation == '/':
+    print('{} / {} = '.format(number_1, number_2))
+    print(number_1 / number_2)
+
+elif operation == 'no':
+    print("get outta here")
+else:
+    print('I said addition, subraction, multiplication, or divison! Please rerun the program')
